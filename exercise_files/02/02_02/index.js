79c8c43e90e6541da3f9871ffab8f0dfa7c4562c
Sandra mada <sandramada@sandras-mbp.home>
2024-04-23 18:34:23 +0200

    02_02

--- exercise_files/02/02_02/index.js
+++ exercise_files/02/02_02/index.js
@@ -1,18 +1,2 @@
 const Replicate = require("replicate");
 require("dotenv").config();
-
-const replicate = new Replicate();
-
-const input = {
-  prompt: "an astronaut riding a horse on mars, hd, dramatic lighting",
-  scheduler: "K_EULER",
-};
-
-(async () => {
-  console.log("generating ...");
-  const output = await replicate.run(
-    "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
-    { input }
-  );
-  console.log(output);
-})();
