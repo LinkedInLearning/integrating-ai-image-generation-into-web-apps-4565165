e4d6f3177c4b711d086db6dfb33ba974bc8661a6
Sandra mada <sandramada@sandras-mbp.home>
2024-04-23 19:21:41 +0200

    02_03

--- exercise_files/02/02_03/index.js
+++ exercise_files/02/02_03/index.js
@@ -1,2 +1,15 @@
-const Replicate = require("replicate");
-require("dotenv").config();
+import Replicate from "replicate";
+import dotenv from "dotenv";
+dotenv.config();
+const replicate = new Replicate();
+
+console.log("Running the model...");
+const output = await replicate.run(
+  "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+  {
+    input: {
+      prompt: "An astronaut riding a rainbow unicorn, cinematic, dramatic",
+    }
+  }
+);
+console.log(output[0]);
